a64cf23657da75a1760b24d3c38b386e0d9497dd
chaimaa morsli <117817198+chaimaa61@users.noreply.github.com>
2024-09-22 23:48:23 +0100

    v1

---
+++ src/main/java/com/inn/cafe/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,7 @@
+package com.inn.cafe.com.inn.cafe.constents;
+
+public class CafeConstants {
+
+    public static final String SOMETHING_WENT_WRONG= "Something Went Wrong.";
+    public static final String INVALID_DATa= "Invalid Data.";
+}
